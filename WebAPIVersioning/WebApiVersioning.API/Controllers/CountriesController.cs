463905c97247d5d26904ce4ca5c60861ffe3dbc3
Arda AY <arda.ay@outlook.com.tr>
2025-03-10 14:19:19 +0300

    Country Model and Controller

---
+++ WebAPIVersioning/WebApiVersioning.API/Controllers/CountriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiVersioning.API.Models.DTOs;
+
+namespace WebApiVersioning.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var countriesDomainModel = CountriesData.Get();
+
+            //Map Domain Model to DTO
+            var response = new List<CountryDto>();
+            foreach (var countryDomain in countriesDomainModel)
+            {
+                response.Add(new CountryDto
+                {
+                    Id = countryDomain.Id,
+                    Name = countryDomain.Name
+                });
+            }
+
+            return Ok(response);
+
+        }
+
+    }
+}
---
+++ WebAPIVersioning/WebApiVersioning.API/CountriesData.cs
@@ -0,0 +1,24 @@
+﻿using WebApiVersioning.API.Models.Domain;
+
+namespace WebApiVersioning.API
+{
+    public class CountriesData
+    {
+
+        public static List<Country> Get()
+        {
+
+            var countries = new[]
+            {
+                new { Id = 1, Name = "United States"},
+                new { Id = 2, Name = "Germany" },
+                new { Id = 3, Name = "Brazil" },
+                new { Id = 4, Name = "China" },
+                new { Id = 5, Name = "Italy" },
+            };
+
+            return countries.Select(c => new Country { Id = c.Id, Name = c.Name }).ToList();
+            
+        }
+    }
+}
